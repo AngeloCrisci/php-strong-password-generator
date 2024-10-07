333427930bb16e6b9e61171a2c18495970a4583c
AngeloCrisci <angelocrisci1998@gmail.com>
2024-10-07 19:29:39 +0200

    First try Milestone 1

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+
+<?php
+
+
+
+
+function generate_password(){
+    $characters = '!"#$%&()*+,-./:;=?@[\]^_{|}~0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $random_string = '';
+    for ($i = 0; $i < $_GET['number']; $i++){
+        $random_string = $characters[rand(0 , strlen($characters))];
+    }
+    return $random_string;
+}
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -12,6 +31,29 @@
     <div class="container">
         <h1 class="text-center mt-5 mb-3"> STRONG PASSWORD GENERATOR</h1>
         <h2 class="text-center">Genera una password sicura</h2>
+        <?php if(isset($_GET['number']) )generate_password();
+        { ?>
+        <div class="smallbox">
+            <p class="p-3"> <?= $random_string ?>  </p>
+        </div>
+        <?php } ?>
+        <div class="big-box">
+            <form action="" method="GET">
+                <div class="row p-3">
+                    <div class="col-6">
+                        Lunghezza PASSWORD:
+                    </div>
+                    <div class="col-6">
+                        <input type="number" name="number">
+                    </div>
+                </div>
+            <div class="p-3">
+                <button type="submit" class="btn btn-primary">INVIA</button>
+                <button type="reset" class="btn btn-danger">ANNULLA</button>
+            </div>
+            </form>
+        </div>
+        
     </div>
 </body>
 </html>
